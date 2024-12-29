b1fba3f1c1bc6326a7c5b2552ea9e346ffbb0f98
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-12-29 14:00:09 +0600

    home page changed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,14 @@
-import Header from "../Header/Header";
+// import Header from "../Header/Header";
 import About from "../HomePageLayout/About/About";
 import Banner from "../HomePageLayout/Banner/Banner";
 import CallUs from "../HomePageLayout/CallUs/CallUs";
 import Album from "../HomePageLayout/Extra/Album/Album";
 import CrazyTeam from "../HomePageLayout/Extra/CazyTeam/CrazyTeam";
-import MyNavBar from "../NavBar/NavBar";
+// import MyNavBar from "../NavBar/NavBar";
 import PetCategory from "../PetCategory/PetCategory";
-import ClipPathCard from "../ProfileCard/ClipPathCard";
-import CustomShapeCard from "../ProfileCard/CustomShapeCard";
-import ProfileCard from "../ProfileCard/ProfileCard";
+// import ClipPathCard from "../ProfileCard/ClipPathCard";
+// import CustomShapeCard from "../ProfileCard/CustomShapeCard";
+// import ProfileCard from "../ProfileCard/ProfileCard";
 
 
 const Home = () => {
@@ -21,11 +21,11 @@ const Home = () => {
             <About></About>
             <Album></Album>
             <CrazyTeam></CrazyTeam>
-       
-            <ProfileCard/>
+
+            {/* <ProfileCard/>
             <ClipPathCard/>
-            <CustomShapeCard/>
-          {/* <Header></Header> */}
+            <CustomShapeCard/> */}
+            {/* <Header></Header> */}
         </div>
     );
 };
